5c9098699b547f97b452afa637e35fa75c5c0d4f
ManuelDiLena <manuel.dilena.29@gmail.com>
2023-05-18 21:11:07 -0300

    Login to the application

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import Home from './components/Home'
 import Login from './components/Login'
+import { getTokenFromURL } from './SpotifyLogic';
 
 function App() {
-  return (
-    <div className="App">
-      {/*<Login />*/}
-      <Home /> 
-    </div>
-  );
+
+    const [token, setToken] = useState('')
+    
+    useEffect(() => {
+        const hash = getTokenFromURL();
+        //window.location.hash = "";
+        const _token = hash.access_token;
+        setToken(_token);
+        console.log('token: ' + _token);
+    }, [])
+
+    return (
+        <div>
+            {
+               token ? <Home /> : <Login />
+            }
+        </div>
+    );
 }
 
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,9 @@
 import React from 'react'
 import { Container, LoginBtn } from '../style/styleLogin'
 import logo from '../images/SpotifyLogo.png'
+import { loginURL } from '../SpotifyLogic'
 
 const Login = () => {
-
-    const endpoint = 'https://accounts.spotify.com/authorize';
-    const clientID = '81bde7e40b454816a65695e478b9e39b';
-    const redirectUri = 'https://localhost:3000/';
-    const scopes = ['user-read-currently-playing',
-        'user-read-recently-played',
-        'user-read-playback-state',
-        'user-top-read',
-        'user-modify-playback-state'];
-
-    // URL with all its parameters to access the app
-    const loginURL = `${endpoint}?client_id=${clientID}&response_type=token&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&show_dialog=true`;
-
-
     return (
         <Container>
             <img src={logo} alt='Spotify'/>
